f7b8361c0e28e8fc2ec4821b2d0e32d004a402c8
Naila <snailaafn@gmail.com>
2024-12-03 14:19:57 +0700

    bugfix #10

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -6,7 +6,7 @@ class DashboardController extends Controller
 {
     public function index()
     {
-        $transaksi_count =
-        return view('dashboard', );
+        $transaksi_count = Transaksi::count();
+        return view('dashboard', compact('transaksi_count') );
     }
 }
