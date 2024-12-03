3716d9103d9a7c00a6e0c4accf3e7b55c3eb27aa
Naila <snailaafn@gmail.com>
2024-12-03 14:45:11 +0700

    bugfix #13

--- resources/views/transaksidetail/index.blade.php
+++ resources/views/transaksidetail/index.blade.php
@@ -17,14 +17,14 @@
                 </tr>
                 </thead>
                 <tbody>
-                @foreach ( as $index => $data)
+                @foreach ( $transaksiDetail as $index => $data)
                     <tr>
                         <td>{{ $index+1 }}</td>
-                        <td>{{ \Carbon\Carbon::parse($data->transaksi->tanggal)->format('d/m/Y') }}</td>
+                        <td>{{ \Carbon\Carbon::parse($data->transaksi->tanggal_pembelian)->format('d/m/Y') }}</td>
                         <td>{{ $data->nama_produk }}</td>
                         <td class="text-end">{{ number_format($data->hargasatuan, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->junlah, 0, '.', '.') }}</td>
-                        <td class="text-end">{{ number_format($data->subt0tal, 0, '.', '.') }}</td>
+                        <td class="text-end">{{ number_format($data->jumlah, 0, '.', '.') }}</td>
+                        <td class="text-end">{{ number_format($data->subtotal, 0, '.', '.') }}</td>
                     </tr>
                 @endforeach
                 </tbody>
