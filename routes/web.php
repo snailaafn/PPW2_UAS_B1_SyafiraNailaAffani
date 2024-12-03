b04e712ade85aba4087bdbd09aad7b08d1ca73ef
Naila <snailaafn@gmail.com>
2024-12-03 13:22:21 +0700

    bugfixing#4

--- routes/web.php
+++ routes/web.php
@@ -21,27 +21,27 @@ Route::get('/', function () {
     return view('welcome');
 });
 
-Route::controller(LoginController::class)-group(function() {
+Route::controller(LoginController::class)->group(function() {
     Route::get('/login', 'login')->name('login');
     Route::post('/authenticate', 'authenticate')->name('authenticate');
     Route::post('/logout', 'logout')->name('logout');
 });
 
-Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard')
+Route::get('/dashboard', [DashboardController::class,'index'])->name('dashboard');
 
-Route:prefix('/transaksi')->group(function () {
+Route::prefix('/transaksi')->group(function () {
     Route::get('/', [TransaksiController::class, 'index'])->name('transaksi.index');
     Route::get('create', [TransaksiController::class, 'create'])->name('transaksi.create');
     Route::post('store', [TransaksiController::class, 'store'])->name('transaksi.store');
     Route::get('edit/{id}', [TransaksiController::class, 'edit'])->name('transaksi.edit');
     Route::put('update/{id}', [TransaksiController::class, 'update'])->name('transaksi.update');
     Route::delete('delete/{id}', [TransaksiController::class, 'destroy'])->name('transaksi.destroy');
-})
+});
 
-Route::prefix('/transaksidetail')->group(function  {
+Route::prefix('/transaksidetail')->group(function  () {
     Route::get('/', [TransaksiDetailController::class, 'index'])->name('transaksidetail.index');
     Route::get('/{id_transaksi}', [TransaksiDetailController::class, 'detail'])->name('transaksidetail.detail');
     Route::get('edit/{id}', [TransaksiDetailController::class, 'edit'])->name('transaksidetail.edit');
     Route::put('update/{id}', [TransaksiDetailController::class, 'update'])->name('transaksidetail.update');
     Route::delete('delete/{id}', [TransaksiDetailController::class, 'destroy'])->name('transaksidetail.destroy');
-})
+});
